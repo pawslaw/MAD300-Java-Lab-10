c4800c79ff8876feed91b05dc943d11b1cfee03d
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2016-10-26 16:16:11 -0400

    Created text nodes for the title and subtitle and created a VBox to store the title text nodes.

--- src/Form.java
+++ src/Form.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Form extends Application {
@@ -10,7 +13,22 @@ public class Form extends Application {
 	}
 	
 	public void start(Stage primaryStage) {
+		// Create the initial StackPane
 		StackPane pane = new StackPane();
+		// Create a form title
+		Text title = new Text("Flying Club Form");
+		// Create a subtitle
+		Text subTitle = new Text("Join the Flying Club today!\n"
+							+"Fill out the form below:");
+		// Create an VBox for the title
+		VBox titleBox = new VBox();
+		titleBox.getChildren().addAll(title, subTitle);
+		
+		
+		
+		
+		
+		pane.getChildren().add(titleBox);
 		
 		primaryStage.setTitle("[MAD300 Java Lab 10]");
         primaryStage.setScene(new Scene(pane, 800, 600));
